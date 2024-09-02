7c71576e536764ce636f6a2921b896475c5f404b
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2024-09-02 10:11:07 +0300

    Add project files.

---
+++ Garanti.Domain/Models/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garanti.Domain.Models
+{
+    //Base entity tum tablolarda bulunacak olan ortak alanlari tutar.
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Garanti.Infrastructure/EF/GarantiContext.cs
@@ -0,0 +1,24 @@
+﻿using Garanti.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garanti.Infrastructure.EF
+{
+    //GarantiContext classi icerisinde EntityFramework ile ilgili ayarlarin, tablolarin ve veritabaninin olusturuldugu class
+    public class GarantiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-EET2RGT;Database=GarantiDB;Trusted_Connection=True;trustServerCertificate=true");
+        }
+
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
